4975c8bd70ad05e8d24288cdab9c44c720c9a61c
Louis Adamian <louis@adamian.net>
2024-12-30 18:00:52 -0500

    add NOAA AWC G-AIRMETs

--- metgeojson.py
+++ metgeojson.py
@@ -2,7 +2,11 @@ import urllib.request
 import json
 import os
 import datetime
-from datetime import datetime
+from datetime import datetime, timezone
+
+
+def utcnow():
+    return datetime.now(timezone.utc).replace(tzinfo=None).isoformat(timespec="minutes")
 
 
 def noaa_coords_to_geojson(coords: [dict]) -> [[float]]:
@@ -13,7 +17,7 @@ def noaa_coords_to_geojson(coords: [dict]) -> [[float]]:
     return new_coords
 
 
-def noaa_2geojson(noaa_json: dict) -> dict:
+def noaa_sigmet2geojson(noaa_json: dict) -> dict:
     features = []
     for met in noaa_json:
         met_geojson = {"type": "Feature"}
@@ -51,29 +55,97 @@ def noaa_2geojson(noaa_json: dict) -> dict:
     geojson_dict = {
         "type": "FeatureCollection",
         "features": features,
-        "metadata": {"creationTime": datetime.utcnow().isoformat(timespec="minutes")},
+        "metadata": {"creationTime": utcnow()},
     }
     return geojson_dict
 
 
-if __name__ == "__main__":
-    usa_url = (
-        "https://aviationweather.gov/api/data/airsigmet?format=json&type=sigmet&date="
-        + datetime.now().isoformat(timespec="minutes").replace(":", "%3A")
-    )
-    usa_filename = "./data/usa_sigmets_current.geojson"
-    if os.path.isfile(usa_filename):
-        newname = "usa_sigmets_"
-        with open(usa_filename) as oldfile:
+def noaa_airmet2geojson(noaa_json: dict) -> dict:
+    features = []
+    for met in noaa_json:
+        met_geojson = {"type": "Feature"}
+        coords = noaa_coords_to_geojson(met.get("coords"))
+        if len(coords) < 1:
+            continue
+        properties = {
+            "forecastId": met.get("icaoId"),
+            "geometryId": met.get("geometryId"),
+            "tag": met.get("tag"),
+            "forecastHour": met.get("forecastHour"),
+            "validTimeFrom": met.get("validTime").replace("Z", ""),
+            "validTimeTo": datetime.fromtimestamp(met.get("expireTime")).isoformat(
+                timespec="minutes"
+            ),
+            "hazard": met.get("hazard"),
+            "frequency": met.get("frequency"),
+            "severity": met.get("severity"),
+            "status": met.get("status"),
+            "base": met.get("base"),
+            "top": met.get("top"),
+            "fzlBase": met.get("fzlbase"),
+            "fzlTop": met.get("fzltop"),
+            "level": met.get("level"),
+            "product": met.get("product"),
+            "rawAirSigmet": met.get("due_to"),
+        }
+        geotype = met.get("geometryType")
+        if geotype == "AREA":
+            geometry = {
+                "type": "Polygon",
+                "coordinates": [coords],
+            }
+        elif geotype == "LINE":
+            geometry = {
+                "type": "LineString",
+                "coordinates": coords,
+            }
+        else:
+            continue
+        met_geojson["geometry"] = geometry
+        met_geojson["properties"] = properties
+        features.append(met_geojson)
+    return {
+        "type": "FeatureCollection",
+        "features": features,
+        "metadata": {"creationTime": utcnow()},
+    }
+
+
+def move_old_file(filename, new_name_prefix, archive_dir):
+    if os.path.isfile(filename):
+        with open(filename) as oldfile:
             old_geojson = json.loads(oldfile.read())
-            newname += old_geojson["metadata"]["creationTime"] + ".geojson"
-        if not os.path.isdir("./data/archive/usa"):
-            os.mkdir("./data/archive/usa")
-        os.rename(usa_filename, "./data/archive/usa/" + newname)
-    contents = urllib.request.urlopen(usa_url).read().decode()
+            new_name_prefix += old_geojson["metadata"]["creationTime"] + ".geojson"
+        if not os.path.isdir("./data/archive"):
+            os.mkdir("./data/archive")
+        if not os.path.isdir("./data/archive/" + archive_dir):
+            os.mkdir("./data/archive/" + archive_dir)
+        os.rename(filename, "./data/archive/" + archive_dir + "/" + new_name_prefix)
+
+
+if __name__ == "__main__":
+    usa_sigmet_url = (
+        "https://aviationweather.gov/api/data/airsigmet?format=json&type=sigmet&date="
+        + utcnow().replace(":", "%3A")
+        + "Z"
+    )
+    usa_sigmet_filename = "./data/usa_sigmets_current.geojson"
+    move_old_file(usa_sigmet_filename, "usa_sigmets_", "usa")
+    contents = urllib.request.urlopen(usa_sigmet_url).read().decode()
     met_json = json.loads(contents)
-    geojson = json.dumps(noaa_2geojson(met_json), indent=4)
-    if not os.path.isdir("./data"):
-        os.mkdir("./data")
-    with open(usa_filename, "x") as geojson_file:
+    geojson = json.dumps(noaa_sigmet2geojson(met_json), indent=4)
+    with open(usa_sigmet_filename, "x") as geojson_file:
+        geojson_file.write(geojson)
+
+    usa_airmet_url = (
+        "https://aviationweather.gov/api/data/gairmet?format=json&date="
+        + utcnow().replace(":", "%3A")
+        + "Z"
+    )
+    usa_airmet_filename = "./data/usa_airmets_current.geojson"
+    move_old_file(usa_airmet_filename, "usa_airmets_", "airmet")
+    contents = urllib.request.urlopen(usa_airmet_url).read().decode()
+    met_json = json.loads(contents)
+    geojson = json.dumps(noaa_airmet2geojson(met_json), indent=4)
+    with open(usa_airmet_filename, "x") as geojson_file:
         geojson_file.write(geojson)
